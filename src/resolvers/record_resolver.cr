656808e324266559b4655b2976bda7848e118d4b
Gary Mardell <info@garymardell.co.uk>
2021-08-17 00:14:33 -0700

    Load cells for all records simultaneously

--- src/resolvers/record_resolver.cr
+++ src/resolvers/record_resolver.cr
@@ -2,47 +2,47 @@ module Resolvers
   class CellLoader
     @@loaders = {} of Int64 => CellLoader
 
-    def self.for(rec : Record)
-      unless @@loaders.has_key?(rec.id)
-        @@loaders[rec.id.not_nil!] = CellLoader.new(rec)
+    def self.for(model : Model)
+      unless @@loaders.has_key?(model.id)
+        @@loaders[model.id.not_nil!] = CellLoader.new(model)
       end
 
-      @@loaders[rec.id.not_nil!]
+      @@loaders[model.id.not_nil!]
     end
 
-    property rec : Record
-    property fields : Array(String)
+    property model : Model
+    property records : Array(Int64)
+    # property fields : Array(String)
 
-    def initialize(rec : Record)
-      @rec = rec
-      @fields = [] of String
-      @cache = {} of String => Graphql::Lazy(String | Nil)
+    def initialize(@model : Model)
+      @records = [] of Int64
+      @cache = {} of Tuple(Int64, String) => Graphql::Lazy(String | Nil)
     end
 
     def resolve
       return if resolved?
 
-      load_keys = @fields
-      @fields = [] of String
+      load_keys = @records
+      @records = [] of Int64
 
       perform(load_keys)
     end
 
-    def perform(load_keys : Array(String))
-      cells = Cell.where(record_id: rec.id, name: load_keys)
+    def perform(load_keys : Array(Int64))
+      cells = Cell.where(record_id: load_keys.uniq)
       cells.each do |cell|
-        fulfill(cell.name, cell.data)
+        fulfill({ cell.record_id, cell.name }, cell.data)
       end
     end
 
     def resolved?
-      @fields.empty?
+      @records.empty?
     end
 
-    def load(field : String)
-      fields << field
+    def load(id : Int64, field : String)
+      records << id
 
-      @cache[field] ||= Graphql::Lazy(String | Nil).new {
+      @cache[{id, field}] ||= Graphql::Lazy(String | Nil).new {
         resolve
       }
     end
@@ -61,8 +61,8 @@ module Resolvers
       if field_name == "id"
         r.id
       else
-        loader = CellLoader.for(r)
-        loader.load(field_name)
+        loader = CellLoader.for(r.model)
+        loader.load(r.id.not_nil!, field_name)
       end
     end
   end
